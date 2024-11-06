3dea01d27979e9ed44672e552386480dd93295d6
paciadawid <paciadawid@gmail.com>
2024-11-06 14:08:30 +0100

    init

--- test_run.py
+++ test_run.py
@@ -13,4 +13,13 @@ options.app = app_path
 
 driver = webdriver.Remote(command_executor=appium_server_url, options=options)
 
-driver.find_element(AppiumBy.ID, 'formula')
+
+
+driver.find_element(AppiumBy.ACCESSIBILITY_ID, '2').click()
+driver.find_element(AppiumBy.CSS_SELECTOR, '[resource-id="op_add"]').click()
+driver.find_element(AppiumBy.ACCESSIBILITY_ID, '1').click()
+driver.find_element(AppiumBy.XPATH, '//*[@content-desc="equals"]').click()
+
+result = driver.find_element(AppiumBy.ID, 'result_final').text
+
+assert int(result) == 3, f'Error: {result} is not 3'
