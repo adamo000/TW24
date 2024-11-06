9c8a8798f02d7bbbf0e0f785a021212e6f86b4d8
paciadawid <paciadawid@gmail.com>
2024-11-06 11:16:25 +0100

    init

--- test_run.py
+++ test_run.py
@@ -3,12 +3,11 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 
 app_name = "calculator.apk"
-app_path = str(Path(__file__).parent.parent.joinpath(app_name))
+app_path = str(Path(__file__).parent.joinpath(app_name))
 appium_server_url = 'http://localhost:4723'
 
 options = UiAutomator2Options() 
 options.platform_name = "Android" 
-options.platform_version = "13"
 options.app = app_path
 
 driver = webdriver.Remote(command_executor=appium_server_url, options=options)
